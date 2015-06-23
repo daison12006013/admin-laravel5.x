8234313d7055c8d95214f91f51c7d76ca29d55bd
Daison Carino <daison12006013@gmail.com>
2015-06-23 17:58:13 +0000

    just updating the contents

---
+++ daison12006013/src/AdminLaravelServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Daison\AdminLaravelFiveX;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminLaravelServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        if (! $this->app->routesAreCached()) {
+            require __DIR__.'/../../routes.php';
+        }
+
+        $this->base_folder = dirname(__FILE__);
+
+        $this->initViews($this->base_folder);
+        $this->initTranslations($this->base_folder);
+        $this->initConfig($this->base_folder);
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            $base_folder . '/app/config/init.php', 'admin'
+        );
+    }
+
+    public function initViews($base_folder)
+    {
+        $this->loadViewsFrom($base_folder . '/resources/views', 'admin');
+        $this->publishes([
+            $base_folder . '/resources/views' => 
+                base_path('resources/views/vendor/admin-laravel5.x'),
+        ]);
+    }
+
+    public function initTranslations($base_folder)
+    {
+        $this->loadTranslationsFrom($base_folder . '/resources/lang', 'admin');
+    }
+
+    public function initConfig($base_folder)
+    {
+        $this->publishes([
+            $base_folder . '/app/config/init.php' => 
+                config_path('admin-laravel5.x/init.php'),
+        ]);
+    }
+}
